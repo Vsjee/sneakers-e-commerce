07ba29a81ee4a8313dd7020d8f5ac381e088229f
david hernandez <davidfelipehernandez1@gmail.com>
2022-10-12 18:25:02 -0500

    footer comp to the bottom

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FooterWrapper from '@/styled-components/Footer/FooterWrapper'
+
+const Footer = () => {
+  return (
+    <FooterWrapper>
+      <h1>I'm the footer</h1>
+    </FooterWrapper>
+  )
+}
+
+export default Footer
