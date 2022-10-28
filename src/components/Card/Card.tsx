c4e28431c002715673bec39ab89ea13719be5766
david hernandez <davidfelipehernandez1@gmail.com>
2022-10-28 10:51:55 -0500

    refactor Card comp and accept props

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,53 +1,60 @@
-import { data } from '@/data';
 import centsToUsd from '@/utilities/centsToUsd';
 import { Link } from 'react-router-dom';
 import Button from '../Button/Button';
+import CardWrapper from './CardWrapper.css';
 
-const Card = () => {
+interface Props {
+  dataItem: {
+    id: number
+    name: string
+    main_picture_url: string
+    nickname: string
+    release_year: number | null
+    retail_price_cents: number | null
+    size_range: number[]
+    category: string[]
+  }
+}
+
+const Card = ({ dataItem }: Props) => {
   return (
-    <>
-      {
-        data.sneakers.map(item => {
-          return (
-            <ul key={item.id}>
-              <li>
-                <Link to={`/products/${item.id}`}>
-                  <figure>
-                    <img src={item.main_picture_url} alt={item.name} width="180px" /> 
-                  </figure>
-                </Link>
-              </li>
-              <li>{item.name}</li>
-              <li>Surname: {item.nickname}</li>
-              <li>Release: {item.release_year}</li>
-              <li>Category: {item.category}</li>
-              <li>
-                {
-                <select name="sizes" id="">
-                  <option value="select">Select</option>
-                  {
-                    item.size_range.map(sized => {
-                      return <option value="" key={sized}>{sized}</option>
-                    })
-                  }
-                </select>
-                }
-              </li>
-              <li>
-                ${
-                  centsToUsd(Number(item?.retail_price_cents))
-                }
-              </li>
-              <li>
-               <Link to="/products/:id">
-                  <Button buttonType=''>Buy</Button>
-                </Link>
-              </li>
-            </ul>
-          )
-        })
-      }
-    </>
+    <CardWrapper>
+      <ul key={dataItem.id}>
+        <li>
+          <Link to={`/products/${dataItem.id}`}>
+            <figure>
+              <img src={dataItem.main_picture_url} alt={dataItem.name} width="180px" />
+            </figure>
+          </Link>
+        </li>
+        <li>{dataItem.name}</li>
+        <li>Surname: {dataItem.nickname}</li>
+        <li>Release: {dataItem.release_year}</li>
+        <li>Category: {dataItem.category}</li>
+        <li>
+          {
+            <select name="sizes">
+              <option value="select">Select</option>
+              {
+                dataItem.size_range.map(sized => {
+                  return <option value="" key={sized}>{sized}</option>
+                })
+              }
+            </select>
+          }
+        </li>
+        <li>
+          ${
+            centsToUsd(Number(dataItem?.retail_price_cents))
+          }
+        </li>
+        <li>
+          <Link to="/products/:id">
+            <Button buttonType=''>Buy</Button>
+          </Link>
+        </li>
+      </ul>
+    </CardWrapper>
   )
 }
 
