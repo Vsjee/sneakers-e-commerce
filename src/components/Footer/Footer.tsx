d3dd41bf44464edaca071b5fcfe4e0711d74d9f3
David Hernandez <davidfelipehernandez1@gmail.com>
2022-11-01 21:22:32 -0500

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,30 @@
 import FooterWrapper from "./FooterWrapper.css"
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 const Footer = () => {
   return (
     <FooterWrapper>
-      <h1>I'm the footer</h1>
+      <h1>
+        Sneak<span className="magic">Shop</span>
+      </h1>
+      <article className="accounts">
+        <ul>
+          <li>
+            <a href="https://github.com/Vsjee" target="_blank"><AiFillGithub className="icon"/></a>
+          </li>
+          <li>
+            <a href=""><AiFillLinkedin className="icon"/></a>
+          </li>
+        </ul>
+      </article>
+      <article className="madeby">
+        <p>
+          Made by <span>David Felipe Hernandez</span>.
+        </p>
+        <p>
+          All reserved rigths © 2022.
+        </p>
+      </article>
     </FooterWrapper>
   )
 }
--- src/components/Footer/FooterWrapper.css.ts
+++ src/components/Footer/FooterWrapper.css.ts
@@ -1,9 +1,60 @@
 import styled from "styled-components";
 
 const FooterWrapper = styled.footer`
+  @keyframes background-pan {
+    from {
+      background-position: 0% center;
+    }
+
+    to {
+      background-position: -200% center;
+    }
+  }
+
+  text-align: center;
   margin-top: auto;
   padding: 1rem;
   background-color: #fc4747;
+
+  h1 > .magic {
+    animation: background-pan 3s linear infinite;
+    background: linear-gradient(
+      90deg, #000, #fff, #000, #fff, #000
+    );
+    background-size: 200%;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    white-space: nowrap;
+  }
+
+  .accounts {
+    ul {
+      display: flex;
+      justify-content: center;
+      gap: 1rem;
+      padding: 1rem;
+      list-style: none;
+
+      li a .icon {
+        color: #000;
+        width: 30px;
+        height: 30px;
+        transition: .5s ease-in-out;
+
+        &:hover {
+          color: #fff;
+        }
+      }
+    }
+  }
+
+  .madeby {
+    font-size: .7rem;
+
+    p > span {
+      font-weight: bold;
+    }
+  }
 `
 
 export default FooterWrapper
