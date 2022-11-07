7261481f6105b33484f600c84ab91d7ade69a42e
david hernandez <davidfelipehernandez1@gmail.com>
2022-11-06 22:08:04 -0500

    responsive navbar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,12 @@
 import sneakerLogo from '../../assets/sneakerLogo.svg'
-import { lazy, Suspense, useState } from 'react'
+import { lazy, Suspense, useState, createRef } from 'react'
 import { Link, Route } from 'react-router-dom'
 import { RoutesWithNotFound } from '@/utilities'
 import Button from '../Button/Button'
 import NavBarWrapper from './NavBarWrapper.css'
 import { ShopCart } from '../ShopCart'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
+import { FaTimes, FaBars } from 'react-icons/fa'
 
 const HomePage = lazy(() => import('../../pages/home/HomePage'))
 const Products = lazy(() => import('../../pages/products/Products'))
@@ -16,6 +17,18 @@ const Contact = lazy(() => import('../../pages/Contact/Contact'))
 const NavBar = () => {
 
   const [toggleDrawer, setToggleDrawer] = useState(false)
+  const [navIcons, setNavIcons] = useState(false)
+
+  const navRef = createRef<HTMLElement>()
+
+  const showIcon = () => {
+    setNavIcons(!navIcons)    
+  }
+
+  const showNavBar = () => {
+    navRef?.current?.classList.toggle('responsive__nav')
+    showIcon()
+  }
 
   return (
     <>
@@ -25,25 +38,35 @@ const NavBar = () => {
             <img src={sneakerLogo} alt="sneaker-logo" width={50} />
           </figure>
         </Link>
-        <nav>
-          <ul>
-            <Link to='/products'>
-              Products
-            </Link>
-            <Link to='/about'>
-              About
-            </Link>
-            <Link to='/contact'>
-              <Button buttonType='primary'>Contact</Button>
-            </Link>
-            <Button onClick={() => setToggleDrawer(true)} buttonType="" className='shopIcon'>
-              <AiOutlineShoppingCart className='shopIcon--icon'/>
-              <div className='shopIcon_counter'>
-                <span>0</span>
-              </div>
-            </Button>
-          </ul>
-        </nav>
+        <div className='container'>
+        <nav className='nav' ref={navRef}>
+            <ul>
+              <Link to='/products' className='nav__link' onClick={showNavBar}>
+                Products
+              </Link>
+              <Link to='/about' className='nav__link' onClick={showNavBar}>
+                About
+              </Link>
+              <Link to='/contact' className='nav__link' onClick={showNavBar}>
+                <Button buttonType='primary' className='nav__link--btn'>Contact</Button>
+              </Link>
+            </ul>
+          </nav>
+              <Button onClick={() => setToggleDrawer(true)} buttonType="" className='shopIcon'>
+                <AiOutlineShoppingCart className='shopIcon--icon'/>
+                <div className='shopIcon_counter'>
+                  <span>0</span>
+                </div>
+              </Button>
+              <Button buttonType='' className='nav__responsive__btn' 
+                onClick={showNavBar}>
+                {
+                  navIcons === true 
+                    ? <FaTimes className='nav__close__btn--icon'/>
+                    : <FaBars className='nav__open__btn--icon'/>
+                }
+              </Button>
+        </div>
       </NavBarWrapper>
       
       {toggleDrawer && (
--- src/components/NavBar/NavBarWrapper.css.ts
+++ src/components/NavBar/NavBarWrapper.css.ts
@@ -13,11 +13,18 @@ const NavBarWrapper = styled.header`
 
   z-index: 1;
 
+  .container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+
   nav ul {
     list-style: none;
     display: flex;
     align-items: center;
     gap: 1rem;
+    margin-left: 200px;
   }
 
   figure img {
@@ -59,6 +66,92 @@ const NavBarWrapper = styled.header`
         position: relative;
         bottom: 3px;
       }
+    }
+  }
+
+  .nav__responsive__btn {
+    position: absolute;
+    left: 85%;
+    background: none;
+    border: none;
+    width: 30px;
+    height: 30px;
+    padding: 0;
+    visibility: hidden; 
+    opacity: 0;
+  }
+
+  .nav__close__btn--icon, .nav__open__btn--icon {
+      color: #fff;
+      width: 25px;
+      height: 25px;
+  }
+
+  @media screen and (max-width: 600px) {
+    img {
+      position: relative;
+      right: 50px;
+    }
+    
+    .shopIcon {
+      position: relative;
+      right: 10px;
+    }
+
+    .nav__responsive__btn {
+      visibility: visible;
+      opacity: 1;
+    }
+
+    .nav {
+      position: fixed;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      background-color: #fc4747;
+      gap: 1.5rem;
+      transition: 1s;
+      transform: translateY(-110vh);
+
+      ul {
+        flex-direction: column;
+        margin: 0;
+        gap: 3rem;
+
+        .nav__link, .nav__link--btn  {
+          color: #000;
+          font-size: 2rem;
+        }
+
+        .nav__link--btn {
+          padding: 5px;
+          background-color: #000;
+          color: #fff;
+        }
+      }
+	  }
+
+    .responsive__nav {
+		  transform: none;
+	  }
+
+    .shopIcon .shopIcon--icon {   
+      color: #fff;
+      
+      &:hover {
+        color: #fff;
+      }
+    }
+    
+    .shopIcon_counter span {
+        padding: 0 5px;
+        border-radius: 50%;
+        color: #fff;
     }
   }
 `
