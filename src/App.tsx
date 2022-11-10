22420f88d4c79fb4affd51e71d50da9ef3f49d83
david hernandez <davidfelipehernandez1@gmail.com>
2022-11-09 19:56:46 -0500

    persist my global state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { lazy, Suspense } from 'react';
-import { Provider } from 'react-redux';
-import { Route } from 'react-router-dom';
 import AppWrapper from './app.style';
 import { Footer, NavBar } from './components';
+import { Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import myStore from './redux/store';
 import { RoutesWithNotFound } from './utilities';
+import { PersistGate } from 'redux-persist/integration/react';
+import persistStore from 'redux-persist/es/persistStore';
 
 const HomePage = lazy(() => import('./pages/home/HomePage'));
 const Products = lazy(() => import('./pages/products/Products'));
@@ -13,24 +15,28 @@ const About = lazy(() => import('./pages/about/About'));
 const Contact = lazy(() => import('./pages/Contact/Contact'));
 const Checkout = lazy(() => import('./pages/checkout/Checkout'));
 
+const persistor = persistStore(myStore);
+
 function App() {
   return (
     <>
       <Provider store={myStore}>
-        <AppWrapper>
-          <NavBar />
-          <Suspense fallback="Loading...">
-            <RoutesWithNotFound>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/products/:id" element={<SneakerProduct />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/checkout" element={<Checkout />} />
-            </RoutesWithNotFound>
-          </Suspense>
-          <Footer />
-        </AppWrapper>
+        <PersistGate persistor={persistor}>
+          <AppWrapper>
+            <NavBar />
+            <Suspense fallback="Loading...">
+              <RoutesWithNotFound>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/products/:id" element={<SneakerProduct />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/checkout" element={<Checkout />} />
+              </RoutesWithNotFound>
+            </Suspense>
+            <Footer />
+          </AppWrapper>
+        </PersistGate>
       </Provider>
     </>
   );
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { cartSlice } from './state/cart';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartSlice.reducer);
 
 const myStore = configureStore({
-  reducer: cartSlice.reducer,
+  reducer: persistedReducer,
 });
 
 export default myStore;
